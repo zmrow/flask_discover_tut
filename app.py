5be8ac516da30e86f786072e6a190f174006540c
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2015-12-15 21:27:12 -0800

    Add sqlite db.  Move templates to take advantage of inheritance with jinja2. Add gitignore. Finish video 5

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # import the Flask class
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from functools import wraps
+import sqlite3
 
 # create the app object
 app = Flask(__name__)
 
 app.secret_key = "my precious"
+app.database = "sample.db"
 
 # Login required decorator
 def login_required(f):
@@ -22,7 +24,11 @@ def login_required(f):
 @app.route('/')
 @login_required
 def home():
-    return render_template('index.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('index.html', posts=posts)
 
 @app.route('/welcome')
 def welcome():
@@ -46,6 +52,9 @@ def logout():
     session.pop('logged_in', None)
     flash('You were just logged out.')
     return redirect(url_for('welcome'))
+
+def connect_db():
+    return sqlite3.connect(app.database)
 
 # start server with run() method
 if __name__ == '__main__':
