85f86b210647da334692776235dd79425a0cd1e5
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2016-01-06 19:01:10 -0800

    Updated config and environment settings for heroku

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 # Config
 import os
 app.config.from_object(os.environ['APP_SETTINGS'])
-print os.environ['APP_SETTINGS']
 
 # create the sqlalchemy object
 db = SQLAlchemy(app)
@@ -56,9 +55,6 @@ def logout():
     session.pop('logged_in', None)
     flash('You were just logged out.')
     return redirect(url_for('welcome'))
-
-#def connect_db():
-#    return sqlite3.connect(app.database)
 
 # start server with run() method
 if __name__ == '__main__':
