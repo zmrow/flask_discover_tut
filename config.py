4aa8de28f55af92591a927b0ccbf4b251071bd03
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2016-01-06 19:58:37 -0800

    Setup postgres

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = '\x99\xda?\x15\xdd\x99\xfc\xed\x10\xe5\xf1Zw\xf4|\x8d\xb5\x8f\xd0\xffO`\xdal'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    print SQLALCHEMY_DATABASE_URI
 
 
 class DevelopmentConfig(BaseConfig):
--- db_create.py
+++ db_create.py
@@ -7,6 +7,7 @@ db.create_all()
 # Insert
 db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m well."))
+db.session.add(BlogPost("postgress", "setup local postgressss"))
 
 # Commit the changes
 db.session.commit()
