40c968b768460bcf1aaf02de793c49ac9d63f351
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2016-01-09 19:27:49 -0800

    Convert app to use blueprints

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
-# import the Flask class
-from flask import Flask, render_template, redirect, url_for, request, session, flash, g
+# imports
+from flask import Flask, render_template, redirect, url_for, flash, session
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.bcrypt import Bcrypt
 from functools import wraps
+import os
 
-# create the app object
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
 
 # Config
-import os
+app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-
-# create the sqlalchemy object
 db = SQLAlchemy(app)
 
 from models import *
+from project.users.views import users_blueprint
+
+app.register_blueprint(users_blueprint)
 
 # Login required decorator
-def login_required(f):
-    @wraps(f)
+def login_required(test):
+    @wraps(test)
     def wrap(*args, **kwargs):
         if 'logged_in' in session:
-            return f(*args, **kwargs)
+            return test(*args, **kwargs)
         else:
             flash('You need to login first.')
-            return redirect(url_for('login'))
+            return redirect(url_for('users.login'))
     return wrap
 
 # use decorators to link the function to a url
@@ -39,25 +37,7 @@ def home():
 def welcome():
     return render_template('welcome.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            flash('You were just logged in.')
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
-@app.route('/logout')
-@login_required
-def logout():
-    session.pop('logged_in', None)
-    flash('You were just logged out.')
-    return redirect(url_for('welcome'))
-
 # start server with run() method
 if __name__ == '__main__':
     app.run()
+
